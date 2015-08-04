e498e35d8dd3bfa5f3a59e14292dea8b19879e71
Bobby Kostadinov <Bobby.Kostadinov@dowjones.com>
2015-08-04 13:20:58 +0100

    readme and gulp

--- gulp/bump.js
+++ gulp/bump.js
@@ -1,20 +1,21 @@
-var git = require('gulp-git'),
-  bump = require('gulp-bump'),
-  filter = require('gulp-filter'),
-  tag_version = require('gulp-tag-version');
+var gulp = require('gulp'),
+    git = require('gulp-git'),
+    bump = require('gulp-bump'),
+    filter = require('gulp-filter'),
+    tag_version = require('gulp-tag-version');
 
-module.exports = function bump(importance, skipTagging) {
+module.exports = function (importance, skipTagging) {
   if (skipTagging === false) {
-    return gulp.src(['./package.json'])
+    return gulp.src(['./../package.json'])
       .pipe(bump({type: importance}))
-      .pipe(gulp.dest('./'))
+      .pipe(gulp.dest('./../'))
       .pipe(git.commit('bumps package version'))
       .pipe(filter('package.json'))
       .pipe(tag_version());
   }
-  return gulp.src(['./package.json'])
+  return gulp.src(['./../package.json'])
     .pipe(bump({type: importance}))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./../'))
     .pipe(git.commit('bumps package version'))
     .pipe(filter('package.json'));
 };
